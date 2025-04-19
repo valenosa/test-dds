9fcb406fbf102819ddb739a54ee29f1fbcc59b84
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-04-18 21:39:19 -0300

    Arreglo de error al crear solicitud de eliminación
    Co-authored-by: Facundo Diez Forradellas FacundoDiezF@users.noreply.github.com

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/solicitudes/SolicitudEliminacionDeHecho.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/solicitudes/SolicitudEliminacionDeHecho.java
@@ -5,35 +5,34 @@ import lombok.Getter;
 
 public class SolicitudEliminacionDeHecho {
 
-    private Integer id;
-    private Hecho hecho;
-    private String justificacion;
-    @Getter
-    private EstadoSolicitud estado;
+  private Integer id;
+  private Hecho hecho;
+  private String justificacion;
+  @Getter
+  private EstadoSolicitud estado;
 
-    //REQUISITO 1
-    public SolicitudEliminacionDeHecho(Hecho hecho, String justificacion) {
-        //REQUISITO 2
-        if (justificacion == null || justificacion.length() < 500) {
-            throw new IllegalArgumentException("La justificación debe tener al menos 500 caracteres.");
-        }
-        this.hecho = hecho;
-        this.justificacion = justificacion;
-        this.estado = EstadoSolicitud.PENDIENTE;
-        hecho.addSolicitudEliminacionDeHecho(this);
+  //REQUISITO 1
+  public SolicitudEliminacionDeHecho(Hecho hecho, String justificacion) {
+    //REQUISITO 2
+    if (justificacion == null || justificacion.length() < 500) {
+      throw new IllegalArgumentException("La justificación debe tener al menos 500 caracteres.");
     }
+    this.hecho = hecho;
+    this.justificacion = justificacion;
+    this.estado = EstadoSolicitud.PENDIENTE;
+  }
 
-    public void rechazar() {
-        this.estado = EstadoSolicitud.RECHAZADA;
-        //TODO actualizar la solicitud en la bbdd
-    }
+  public void rechazar() {
+    this.estado = EstadoSolicitud.RECHAZADA;
+    //TODO actualizar la solicitud en la bbdd
+  }
 
-    //Recordemos que, o bien cuando pasen 24 horas o cuando un admin lo decida, deberiamos activar este metodo.
-    public void aceptar() {
-        this.estado = EstadoSolicitud.ACEPTADA;
-        this.hecho.setEliminado(true);
-        //TODO actualizar la solicitud en la bbdd
-    }
+  //Recordemos que, o bien cuando pasen 24 horas o cuando un admin lo decida, deberiamos activar este metodo.
+  public void aceptar() {
+    this.estado = EstadoSolicitud.ACEPTADA;
+    this.hecho.setEliminado(true);
+    //TODO actualizar la solicitud en la bbdd
+  }
 }
 
 //Map<Id_Hecho, Id_Solicitud>
