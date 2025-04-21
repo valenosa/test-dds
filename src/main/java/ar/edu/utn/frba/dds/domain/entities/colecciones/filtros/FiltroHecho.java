31cde559b76741ae5aa044876353427bd6fffd81
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-04-21 16:17:18 -0300

    todo: FiltroHecho -> Filtro

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/colecciones/filtros/FiltroHecho.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/colecciones/filtros/FiltroHecho.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frba.dds.domain.entities.colecciones.filtros;
 
 import ar.edu.utn.frba.dds.domain.entities.hecho.Hecho;
 
+//TODO Cambiar el nombre de esta interfaz a "Filtro"
 public interface FiltroHecho {
 
   boolean cumple(Hecho hecho);
