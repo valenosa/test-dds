8c6ea1561e1bfc43cbc3e4cf9460d83ce9c55080
Valentín Rodrigo Osa Pochelú <83839175+valenosa@users.noreply.github.com>
2025-04-22 14:56:26 -0300

    Revert "Etiquetas"

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/Etiqueta.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.utn.frba.dds.domain.entities;
-
-public class Etiqueta {
-  private String nombre;
-}
--- src/main/java/ar/edu/utn/frba/dds/domain/entities/fuente/estrategias/Fuente.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.utn.frba.dds.domain.entities.fuente.estrategias;
-
-import ar.edu.utn.frba.dds.domain.entities.hecho.Hecho;
-
-import java.util.Set;
-
-public interface Fuente {
-  Set<Hecho> importHechos();
-}
