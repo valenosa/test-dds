95ca2f62a944ea27caf39388aa46711b647cacea
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-04-23 20:49:02 -0300

    refactor: Fuente - Se unifica la FuenteEstatica con la Fuente -Se guardan los hechos asociados a una fuente dentro de una lista
    Co-authored-By: Mateo Nicolás Hernandez <mateonhernandez@frba.utn.edu.ar>
    Co-authored-By: Valentín Rodrigo Osa Pochelú <valenosa@users.noreply.github.com>

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/fuente/estrategias/Fuente.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/fuente/estrategias/Fuente.java
@@ -1,22 +1,78 @@
 package ar.edu.utn.frba.dds.domain.entities.fuente.estrategias;
 
-import ar.edu.utn.frba.dds.domain.entities.BaseDeDatos;
 import ar.edu.utn.frba.dds.domain.entities.hecho.Hecho;
+import ar.edu.utn.frba.dds.domain.entities.hecho.Origen;
+import com.opencsv.CSVReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.Getter;
 
-public abstract class Fuente {
+public class Fuente {
 
-  public Set<String> nombresHechosAsociados;
+  @Getter
+  public Set<Hecho> hechos;
+  private final String rutaArchivoCsv;
 
-  public abstract Set<Hecho> importHechos();
-
-  public Set<Hecho> getHechosAsociados() {
-    BaseDeDatos db = BaseDeDatos.getInstance();
-    return db.obtenerHechos(nombresHechosAsociados);
+  //--- Constructor
+  public Fuente(String rutaArchivoCsv) {
+    this.rutaArchivoCsv = rutaArchivoCsv;
+    hechos = importHechos();
   }
 
-  protected Set<String> nombresHechos(Set<Hecho> hechosImportados) {
-    return hechosImportados.stream().map(Hecho::getTitulo).collect(Collectors.toSet());
+  //--- Importar Hechos desde CSV
+  private Set<Hecho> importHechos() {
+
+    Set<Hecho> hechos = new HashSet<>();
+
+    try (
+        CSVReader reader = new CSVReader(
+            new InputStreamReader(new FileInputStream(rutaArchivoCsv), StandardCharsets.UTF_8)
+        )
+    ) {
+
+      String[] headers = reader.readNext();
+      String[] fila;
+
+      while ((fila = reader.readNext()) != null) {
+        String titulo = fila[0];
+        String descripcion = fila[1];
+        String categoria = fila[2];
+        Double latitud = Double.parseDouble(fila[3]);
+        Double longitud = Double.parseDouble(fila[4]);
+        LocalDate fechaAcontecimiento = null;
+        LocalDate fechaCarga = LocalDate.now();
+        Origen origen = Origen.DATASET;
+
+        try {
+          fechaAcontecimiento = LocalDate.parse(fila[5], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } catch (Exception e) {
+          System.out.println("Fecha inválida para fila: " + Arrays.toString(fila));
+        }
+
+
+        Hecho hecho = new Hecho(
+            titulo,
+            descripcion,
+            categoria,
+            latitud,
+            longitud,
+            fechaAcontecimiento,
+            fechaCarga,
+            origen);
+
+        hechos.add(hecho);
+      }
+    } catch (Exception e) {
+      e.printStackTrace(); //TODO Cambiar esto por un método de logging más robusto
+    }
+
+    return hechos;
   }
 }
