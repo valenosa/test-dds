37c0c05e7a7713ca1d469303bc3dc486d41e257c
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-04-23 13:48:17 -0300

    fix: Elimino clases sin utilización

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/excepciones/HechoEliminadoException.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frba.dds.domain.entities.excepciones;
-
-public class HechoEliminadoException extends RuntimeException {
-  public HechoEliminadoException() {
-    super("No se puede agregar el hecho porque está eliminado.");
-  }
-}
-
