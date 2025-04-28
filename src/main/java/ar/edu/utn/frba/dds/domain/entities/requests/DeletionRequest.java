eb73cb76ffa3c250281820652b24b1630b0a0908
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-04-28 16:14:19 -0300

    refactor: simplificar manejo de metadata
    - Eliminada la clase `DeletionRequestMetadata` y movida su lógica a `DeletionRequest` ya que pareciera ser una Data Class.
    - Renombrado método `isJustificated` a `isJustified` para mejorar legibilidad.
    - Consolidada lógica de evaluación y registro en un solo lugar dentro de `DeletionRequest`.

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/requests/DeletionRequest.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/requests/DeletionRequest.java
@@ -3,39 +3,50 @@ package ar.edu.utn.frba.dds.domain.entities.requests;
 import ar.edu.utn.frba.dds.domain.entities.event.Event;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 public class DeletionRequest {
 
-  @Getter
-  private final Event event;
-  @Getter
-  private final String justification;
-  @Getter
-  private DeletionRequestState state;
-  private final DeletionRequestMetadata metadata;
+  @Getter private final Event event;
+  @Getter private final String justification;
+  @Getter private DeletionRequestState state;
+
+  private final LocalDate uploadDate;
+  private LocalDate evaluationDate;
+
+  private final String applicantName; //TODO luego será un usuario
+  private String evaluatorName; //TODO luego será un usuario
 
   public DeletionRequest(Event event, String justification, String applicantName) {
 
-    if (!this.isJustificated(justification)) {
+    if (!this.isJustified(justification)) {
       throw new IllegalArgumentException("La justificación debe tener al menos 500 caracteres.");
     }
     this.event = event;
     this.justification = justification;
     this.state = DeletionRequestState.PENDING;
-    this.metadata = new DeletionRequestMetadata(applicantName);
+
+    this.uploadDate = LocalDate.now();
+    this.applicantName = applicantName;
   }
 
-  private boolean isJustificated(String justification) {
+  private boolean isJustified(String justification) {
     return justification.length() >= 500;
   }
 
   public void accept(String evaluatorName) {
-    this.metadata.registerEvaluation(evaluatorName);
+    this.registerEvaluation(evaluatorName);
     this.state = DeletionRequestState.ACCEPTED;
     event.setDeleted(true);
   }
 
   public void reject(String evaluatorName) {
-    this.metadata.registerEvaluation(evaluatorName);
+    this.registerEvaluation(evaluatorName);
     this.state = DeletionRequestState.REJECTED;
+  }
+
+  private void registerEvaluation(String evaluatorName) {
+    this.evaluationDate = LocalDate.now();
+    this.evaluatorName = evaluatorName;
   }
 }
