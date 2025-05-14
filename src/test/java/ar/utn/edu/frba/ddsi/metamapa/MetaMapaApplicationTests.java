0fbd9f6cd727c18db3ed03574cd95dbe5610e925
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-13 21:16:16 -0300

    Migración Spring Boot

---
+++ src/test/java/ar/utn/edu/frba/ddsi/metamapa/MetaMapaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.utn.edu.frba.ddsi.metamapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaMapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
