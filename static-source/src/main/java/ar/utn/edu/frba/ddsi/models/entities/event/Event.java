02112a8df60074c3c03fe421d6cb3d2994f0de25
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-14 13:19:23 -0300

    refactor: Event
    - Copio event (Event, Origin, Tag, Category) del proyecto antiguo

---
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/models/entities/event/Event.java
@@ -0,0 +1,57 @@
+package ar.utn.edu.frba.ddsi.models.entities.event;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+public class Event {
+
+  //-- Description
+  @Getter
+  private final String title;
+  private final String description;
+  private final Category category;
+  //-- Ubicacion
+  private final Double latitude;
+  private final Double longitude;
+  //-- Fechas
+  private final LocalDate eventDate;
+  private final LocalDate uploadDate;
+  //-- Extras
+  @Setter
+  private boolean deleted;
+  private final Origin origin;
+  public Set<Tag> tags; //TODO pasar a private
+
+  public Event(String title,
+               String description,
+               Category category,
+               Double latitude,
+               Double longitude,
+               LocalDate eventDate,
+               LocalDate uploadDate,
+               Origin origin) {
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.eventDate = eventDate;
+    this.uploadDate = uploadDate;
+    this.origin = origin;
+    this.deleted = false;
+    tags = new HashSet<>();
+  }
+
+  public void addTag(Tag tag, Tag... tagsAdded) {
+    tags.add(tag);
+    if (tagsAdded != null) {
+      tags.addAll(Set.of(tagsAdded));
+    }
+  }
+
+}
---
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/models/entities/event/Tag.java
@@ -0,0 +1,13 @@
+package ar.utn.edu.frba.ddsi.models.entities.event;
+
+import lombok.Getter;
+
+@Getter
+public class Tag {
+  private final String name;
+
+  public Tag(String name) {
+    this.name = name;
+  }
+
+}
