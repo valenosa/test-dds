3e056dcfec26d0a78ce8b130a8e721fbf5fc7d45
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-16 16:05:13 -0300

    feat: DeletionRequestEvaluationDTO
    - se crea el dto utilizado para el accept y reject de la deletionRequest

--- static-source/src/main/java/ar/utn/edu/frba/ddsi/services/impl/DeletionRequestService.java
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/services/impl/DeletionRequestService.java
@@ -1,6 +1,7 @@
 package ar.utn.edu.frba.ddsi.services.impl;
 
 import ar.utn.edu.frba.ddsi.exceptions.NotFoundException;
+import ar.utn.edu.frba.ddsi.models.dtos.input.DeletionRequestEvaluationDTO;
 import ar.utn.edu.frba.ddsi.models.dtos.input.DeletionRequestInputDTO;
 import ar.utn.edu.frba.ddsi.models.entities.request.DeletionRequest;
 import ar.utn.edu.frba.ddsi.models.entities.request.DeletionRequestState;
@@ -21,27 +22,27 @@ public class DeletionRequestService implements IDeletionRequestService {
 
 
   @Override
-  public void accept(Long deletionRequestId, String evaluatorName) {
+  public void accept(DeletionRequestEvaluationDTO deletionRequestEvaluationDTO) {
     //TODO: Validar si el usuario puede realizar esta peticion
 
-    DeletionRequest deletionRequest = deletionRequestRepository.getById(deletionRequestId);
-    if (deletionRequest == null) throw new NotFoundException("Deletion request not found - ID: " + deletionRequestId);
+    DeletionRequest deletionRequest = deletionRequestRepository.getById(deletionRequestEvaluationDTO.getDeletionRequestId());
+    if (deletionRequest == null) throw new NotFoundException("Deletion request not found - ID: " + deletionRequestEvaluationDTO.getDeletionRequestId());
 
     eventRepository.delete(deletionRequest.getEventId());
-    deletionRequest.registerEvaluation(evaluatorName);
+    deletionRequest.registerEvaluation(deletionRequestEvaluationDTO.getEvaluatorName());
     deletionRequest.setState(DeletionRequestState.ACCEPTED);
 
     deletionRequestRepository.save(deletionRequest);
   }
 
   @Override
-  public void reject(Long deletionRequestId, String evaluatorName) {
+  public void reject(DeletionRequestEvaluationDTO deletionRequestEvaluationDTO) {
     //TODO: Validar si el usuario puede realizar esta peticion
 
-    DeletionRequest deletionRequest = deletionRequestRepository.getById(deletionRequestId);
-    if (deletionRequest == null) throw new NotFoundException("Deletion request not found - ID: " + deletionRequestId);
+    DeletionRequest deletionRequest = deletionRequestRepository.getById(deletionRequestEvaluationDTO.getDeletionRequestId());
+    if (deletionRequest == null) throw new NotFoundException("Deletion request not found - ID: " + deletionRequestEvaluationDTO.getDeletionRequestId());
 
-    deletionRequest.registerEvaluation(evaluatorName);
+    deletionRequest.registerEvaluation(deletionRequestEvaluationDTO.getEvaluatorName());
     deletionRequest.setState(DeletionRequestState.REJECTED);
 
     deletionRequestRepository.save(deletionRequest);
