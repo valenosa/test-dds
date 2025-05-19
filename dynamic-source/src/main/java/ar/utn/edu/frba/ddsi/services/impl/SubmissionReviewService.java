c9a3b005742b9581ceb3148687e6c93bac700239
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-19 20:05:31 -0300

    feat: submissionEvaluation
    -Ahora se puede modificar el estado del hecho (eveluate)
    
    (Falta hacer el controller con el endpoint)

---
+++ dynamic-source/src/main/java/ar/utn/edu/frba/ddsi/services/impl/SubmissionReviewService.java
@@ -0,0 +1,31 @@
+package ar.utn.edu.frba.ddsi.services.impl;
+
+import ar.utn.edu.frba.ddsi.exceptions.NotFoundException;
+import ar.utn.edu.frba.ddsi.models.dtos.input.SubmissionEvaluationDTO;
+import ar.utn.edu.frba.ddsi.models.entities.event.Event;
+import ar.utn.edu.frba.ddsi.models.repositories.IEventRepository;
+import ar.utn.edu.frba.ddsi.services.ISubmissionReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubmissionReviewService implements ISubmissionReviewService {
+
+  @Autowired
+  IEventRepository eventRepository;
+
+
+  @Override
+  public void evaluate(SubmissionEvaluationDTO submissionEvaluation) {
+
+    //TODO: Validar si el usuario puede realizar esta peticion
+
+    Event event = eventRepository.findById(submissionEvaluation.getEventId());
+    if(event == null) throw new NotFoundException("Event not found - ID: " + submissionEvaluation.getEventId());
+
+    event.setState(submissionEvaluation.getSubmissionState());
+    event.setSuggestion(submissionEvaluation.getSuggestion());
+
+    eventRepository.save(event);
+  }
+}
