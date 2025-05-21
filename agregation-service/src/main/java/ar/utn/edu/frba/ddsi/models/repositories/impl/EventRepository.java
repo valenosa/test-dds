801651f5ccc443c66711e0fd04736163691252f3
Matehdez <mateonhernandez@outlook.com>
2025-05-21 14:27:17 -0300

    feat: Creacion del modulo Servicio de Agregacion
    - Creacion de carpetas necesarias para la estructura del nuevo modulo.
      - Se agregan el Event y su repository (cpy pste), modificando poco su estructura
    
    Co-authored-By: Lucas Nahuel Devecchi Di Bella <ldevecchi@frba.utn.edu.ar>
    Co-authored-By: Facundo Diez Forradellas <FacundoDiezF@users.noreply.github.com>

---
+++ agregation-service/src/main/java/ar/utn/edu/frba/ddsi/models/repositories/impl/EventRepository.java
@@ -0,0 +1,42 @@
+package ar.utn.edu.frba.ddsi.models.repositories.impl;
+
+import ar.utn.edu.frba.ddsi.models.entities.event.Event;
+import ar.utn.edu.frba.ddsi.models.repositories.IEventRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class EventRepository implements IEventRepository {
+  Map<Long, Event> events = new HashMap<>();
+  private final AtomicLong idGenerator = new AtomicLong(1);
+
+  @Override
+  public void save(Event event) {
+    if (event.getId() == null) {
+      Long id = idGenerator.getAndIncrement();
+      event.setId(id);
+      events.put(id, event);
+    } else {
+      events.put(event.getId(), event);
+    }
+  }
+
+
+  @Override
+  public List<Event> findAll() {
+    return new ArrayList<>(events.values());
+  }
+
+
+  @Override
+  public List<Event> findByDeleted(boolean deleted) {
+    return events.values().stream().filter(e -> e.isDeleted() == deleted).toList();
+  }
+
+  public Event findById(Long eventId) {
+    return events.get(eventId);
+  }
+
+}
