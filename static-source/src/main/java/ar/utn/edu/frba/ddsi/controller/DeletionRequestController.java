e92e53b027b757cdc867c784ed9888a1dca80976
Lucas Nahuel Devecchi Di Bella <ldevecchi@frba.utn.edu.ar>
2025-05-21 13:21:28 -0300

    fix: DeletionRequestController -Agregados los endpoints necesarios y cambios en el endpoint de deletion-requests a submissions. Co-authored-By: Mateo Nicolás Hernandez <mateonhernandez@frba.utn.edu.ar>

--- static-source/src/main/java/ar/utn/edu/frba/ddsi/controller/DeletionRequestController.java
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/controller/DeletionRequestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/deletion-requests")
+@RequestMapping("/api/submissions")
 public class DeletionRequestController {
 
   private final DeletionRequestService deletionRequestService;
@@ -20,12 +20,12 @@ public class DeletionRequestController {
     this.deletionRequestService = deletionRequestService;
   }
 
-  @PostMapping
+  @PostMapping("/api/submissions")
   public DeletionRequestOutputDTO create(@RequestBody DeletionRequestCreationDTO dto) {
     return deletionRequestService.create(dto);
   }
 
-  @PutMapping
+  @PutMapping("/api/submissions")
   public DeletionRequestOutputDTO evaluate(@RequestBody DeletionRequestEvaluationDTO dto) {
     return deletionRequestService.evaluate(dto);
   }
