ec78850e04e4f2a1f8b38fa07177a498d76408d0
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-22 20:44:27 -0300

    feat: EventOutputDTO ahora tiene el sourceId

--- static-source/src/main/java/ar/utn/edu/frba/ddsi/models/dtos/output/EventOutputDTO.java
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/models/dtos/output/EventOutputDTO.java
@@ -13,6 +13,8 @@ import java.util.stream.Collectors;
 @Data
 public class EventOutputDTO {
   Long id;
+  Long sourceId;
+  Origin origin;
   String title;
   String description;
   String category;
@@ -20,12 +22,13 @@ public class EventOutputDTO {
   Double longitude;
   LocalDate eventDate;
   LocalDate uploadDate;
-  Origin origin;
   Set<String> tags;
 
   public static EventOutputDTO from(Event event) {
     EventOutputDTO dto = new EventOutputDTO();
     dto.setId(event.getId());
+    dto.setSourceId(event.getSourceId());
+    dto.setOrigin(event.getOrigin());
     dto.setTitle(event.getTitle());
     dto.setDescription(event.getDescription());
     dto.setCategory(event.getCategory().getName()); //TODO: Verificar si esta bien pasar nombre o tenemos que pasar Id o un DTO
@@ -33,7 +36,6 @@ public class EventOutputDTO {
     dto.setLongitude(event.getLongitude());
     dto.setEventDate(event.getEventDate());
     dto.setUploadDate(event.getUploadDate());
-    dto.setOrigin(event.getOrigin()); //TODO: Verificar si esta bien pasar el ENUM
     dto.setTags(event.getTags().stream().map(Tag::getName).collect(Collectors.toSet())); //TODO: Verificar si esta bien pasar Set<nombre> o tenemos que pasar Set<Id> o un Set<DTO>
     return dto;
   }
