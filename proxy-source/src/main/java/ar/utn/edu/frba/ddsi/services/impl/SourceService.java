a4ac4169184c726b111883ac305e1f8b25b26daa
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-24 16:18:40 -0300

    refactor: actualizar lógica en getEvents
    - Cambiado el método `getEvents` para recibir un parámetro `LocalDateTime lastUpdate`.
    - Reemplazado el uso de `fetchEvents` por `importEvents` dentro del método.
    - Modificada la firma del método en `ISourceService` para reflejar el cambio de nueva dependencia con `lastUpdate`.
    
    Co-authored-By: Mateo Nicolás Hernandez <mateonhernandez@frba.utn.edu.ar>

--- proxy-source/src/main/java/ar/utn/edu/frba/ddsi/services/impl/SourceService.java
+++ proxy-source/src/main/java/ar/utn/edu/frba/ddsi/services/impl/SourceService.java
@@ -1,12 +1,12 @@
 package ar.utn.edu.frba.ddsi.services.impl;
 
 import ar.utn.edu.frba.ddsi.models.dtos.output.EventOutputDTO;
-import ar.utn.edu.frba.ddsi.models.entities.event.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ar.utn.edu.frba.ddsi.repositories.ISourceRepository;
 import ar.utn.edu.frba.ddsi.services.ISourceService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,13 +16,11 @@ public class SourceService implements ISourceService {
   ISourceRepository sourceRepository;
 
   @Override
-  public List<EventOutputDTO> getEvents() {
-    return sourceRepository.findAll()
+  public List<EventOutputDTO> getEvents(LocalDateTime lastUpdate) {
+    return sourceRepository.findAll() //? Que onda con las proxy MetaMapa
         .stream()
         //Obtengo todos los elementos de todas las fuentes
-        .flatMap(source -> source.fetchEvents().stream())
-        //Filtro los que hayan sido modificados
-        .filter(Event::isModified)
+        .flatMap(source -> source.importEvents(lastUpdate).stream())
         //Los transformo a DTO
         .map(EventOutputDTO::from)
         .toList();
