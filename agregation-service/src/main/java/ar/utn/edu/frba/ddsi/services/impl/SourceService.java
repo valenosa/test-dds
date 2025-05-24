62bc31cd8fcfb98b2a3a84af1fe6bcb4c41851e7
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-24 19:39:28 -0300

    refactor: init y refresh sources
    - Se delega la request y el guardado utilizando una funcion lambda para construir el .uri
    
    Co-authored-By: Valentín Rodrigo Osa Pochelú <valenosa@users.noreply.github.com>

--- agregation-service/src/main/java/ar/utn/edu/frba/ddsi/services/impl/SourceService.java
+++ agregation-service/src/main/java/ar/utn/edu/frba/ddsi/services/impl/SourceService.java
@@ -1,18 +1,19 @@
 package ar.utn.edu.frba.ddsi.services.impl;
 
 import ar.utn.edu.frba.ddsi.config.WebClientConfig;
-import ar.utn.edu.frba.ddsi.models.dtos.input.EventInputDTO;
 import ar.utn.edu.frba.ddsi.models.entities.event.Event;
 import ar.utn.edu.frba.ddsi.models.entities.response.GetEventsResponse;
 import ar.utn.edu.frba.ddsi.models.repositories.IEventRepository;
 import ar.utn.edu.frba.ddsi.services.ISourceService;
 import jakarta.annotation.PostConstruct;
+import java.net.URI;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -27,34 +28,35 @@ public class SourceService implements ISourceService {
   @Override
   @PostConstruct
   public void initSources() {
-    List<WebClient> sourceClients = webClientConfig.getAllClients(); //devuelve 3 webclient, debo hacerles la peti
-
-    for (WebClient sourceClient : sourceClients) {
-      GetEventsResponse getResponse = getEventRequest(sourceClient);
-
-      // Transformo y guardo los eventos
-      getResponse.getEvents()
-          .stream()
-          .map(Event::from)
-          .forEach(eventRepository::save);
-
-      //TODO Manejar GetEventsResponse error
+    for (WebClient sourceClient : webClientConfig.getAllClients()) {
+      fetchAndSaveEvents(sourceClient, uriBuilder -> uriBuilder.path("/events").build());
     }
-  }
-
-  private GetEventsResponse getEventRequest(WebClient sourceClient) {
-    return sourceClient.get().
-        uri("/events")
-        .retrieve()
-        .onStatus(HttpStatusCode::isError, response -> Mono.error(new RuntimeException("Error HTTP: " + response.statusCode())))  //manejo de error HTTP
-        //Recibo en forma de EventInputDTO
-        .bodyToMono(GetEventsResponse.class)
-        .block();
   }
 
   @Override
   public void refreshSources(LocalDateTime lastUpdate) {
-    //Lo mismo que la otra pero con query lastUpdate
+    for (WebClient sourceClient : webClientConfig.getAllClients()) {
+      fetchAndSaveEvents(sourceClient, uriBuilder ->
+          uriBuilder.path("/events").queryParam("lastUpdate", lastUpdate).build());
+    }
+  }
+
+  private void fetchAndSaveEvents(WebClient sourceClient, Function<UriBuilder, URI> uriFunction) {
+    GetEventsResponse getResponse = sourceClient.get()
+        .uri(uriFunction)
+        .retrieve()
+        //Manejo de error en la request
+        .onStatus(HttpStatusCode::isError, response ->
+            Mono.error(new RuntimeException("Error HTTP: " + response.statusCode())))
+        .bodyToMono(GetEventsResponse.class)
+        .block();
+
+    if (getResponse != null && getResponse.getEvents() != null) {
+      getResponse.getEvents()
+          .stream()
+          .map(Event::from)
+          .forEach(eventRepository::save);
+    }
   }
 }
 
