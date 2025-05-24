967543c6a636c81a69252b698bc10a0c7acc08f8
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-24 16:22:41 -0300

    Feat: Agrego POST/collections para crear una coleccion a partir de una CollectionCreatioDTO

--- agregation-service/src/main/java/ar/utn/edu/frba/ddsi/controllers/CollectionController.java
+++ agregation-service/src/main/java/ar/utn/edu/frba/ddsi/controllers/CollectionController.java
@@ -1,10 +1,13 @@
 package ar.utn.edu.frba.ddsi.controllers;
 
+import ar.utn.edu.frba.ddsi.models.dtos.input.CollectionCreationDTO;
 import ar.utn.edu.frba.ddsi.models.dtos.output.CollectionOutputDTO;
 import ar.utn.edu.frba.ddsi.models.dtos.output.EventOutputDTO;
 import ar.utn.edu.frba.ddsi.services.impl.CollectionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,11 @@ public class CollectionController {
     @GetMapping
     public List<CollectionOutputDTO> getCollections(){
         return collectionService.getCollections();
+    }
+
+    @PostMapping
+    public void createCollection(@RequestBody CollectionCreationDTO collectionCreationDTO) {
+        collectionService.create(collectionCreationDTO);
     }
 
     @GetMapping("/{handler}/events")
