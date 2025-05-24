39304b63cee4817cce098e4b715f700a7cb04ba7
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-24 16:12:12 -0300

    fix: Interfaz de EventSource
    Co-authored-By: Mateo Nicolás Hernandez <mateonhernandez@frba.utn.edu.ar>

--- proxy-source/src/main/java/ar/utn/edu/frba/ddsi/models/entities/source/IEventSource.java
+++ proxy-source/src/main/java/ar/utn/edu/frba/ddsi/models/entities/source/IEventSource.java
@@ -2,8 +2,9 @@ package ar.utn.edu.frba.ddsi.models.entities.source;
 
 import ar.utn.edu.frba.ddsi.models.entities.event.Event;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface IEventSource {
-  Set<Event> fetchEvents();
+  Set<Event> importEvents(LocalDateTime lastUpdate);
 }
