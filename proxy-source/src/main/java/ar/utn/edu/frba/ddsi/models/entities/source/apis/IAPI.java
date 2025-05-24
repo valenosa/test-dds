0030ae4129bf3abb8d3fe0cb51d4320111b5d977
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-24 16:11:01 -0300

    fix: Interfaz de la API
    Co-authored-By: Mateo Nicolás Hernandez <mateonhernandez@frba.utn.edu.ar>

--- proxy-source/src/main/java/ar/utn/edu/frba/ddsi/models/entities/source/apis/IAPI.java
+++ proxy-source/src/main/java/ar/utn/edu/frba/ddsi/models/entities/source/apis/IAPI.java
@@ -1,10 +1,11 @@
 package ar.utn.edu.frba.ddsi.models.entities.source.apis;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import ar.utn.edu.frba.ddsi.models.entities.event.Event;
 
 public interface IAPI {
-  Set<Event> importEvents();
+  Set<Event> importEvents(LocalDateTime lastUpdate);
 }
 
 
