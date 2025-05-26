9c1362efc3e69b9a4b3f5c4953aa01532f57b067
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-26 15:26:16 -0300

    style: Actualizar request mapping de SourceController a /sources

--- static-source/src/main/java/ar/utn/edu/frba/ddsi/controller/SourceController.java
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/controller/SourceController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/source")
+@RequestMapping("/sources")
 public class SourceController {
 
   private final SourceService sourceService;
