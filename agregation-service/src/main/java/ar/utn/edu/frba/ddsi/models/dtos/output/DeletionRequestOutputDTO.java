54b3a0f3a372238ccc9abc4cf86b55d0c263c366
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-26 19:28:44 -0300

    Feat: completo DeletionRequestOutputDTO
    Co-authored-By: Lucas Nahuel Devecchi Di Bella <ldevecchi@frba.utn.edu.ar>

--- agregation-service/src/main/java/ar/utn/edu/frba/ddsi/models/dtos/output/DeletionRequestOutputDTO.java
+++ agregation-service/src/main/java/ar/utn/edu/frba/ddsi/models/dtos/output/DeletionRequestOutputDTO.java
@@ -1,14 +1,24 @@
 package ar.utn.edu.frba.ddsi.models.dtos.output;
 
 import ar.utn.edu.frba.ddsi.models.entities.request.DeletionRequest;
+import ar.utn.edu.frba.ddsi.models.entities.request.DeletionRequestState;
 import lombok.Data;
 
 @Data
 public class DeletionRequestOutputDTO {
+  Long id;
+  Long eventId;
+  String argument;
+  DeletionRequestState state;
 
-  public static DeletionRequestOutputDTO from(DeletionRequest deletionRequest){
-
-      return new DeletionRequestOutputDTO();
+  public DeletionRequestOutputDTO(Long id, Long eventId, String argument, DeletionRequestState state) {
+    this.id = id;
+    this.eventId = eventId;
+    this.argument = argument;
+    this.state = state;
   }
-  //TODO: Completar que info necesita
+
+  public static DeletionRequestOutputDTO from(DeletionRequest dr){
+      return new DeletionRequestOutputDTO(dr.getId(),dr.getEventId(),dr.getArgument(),dr.getState());
+  }
 }
