be48b81f8a8cf4c0bda519db27d69916c3479363
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-26 11:41:20 -0300

    fix: Desacoplar unit tests de EventRepositoryTest
    - Desacoplar tests, permitiendo que el resultado no dependa del orden de ejecución

--- static-source/src/test/java/ar/utn/edu/frba/ddsi/models/repositories/impl/EventRepositoryTest.java
+++ static-source/src/test/java/ar/utn/edu/frba/ddsi/models/repositories/impl/EventRepositoryTest.java
@@ -6,6 +6,7 @@ import ar.utn.edu.frba.ddsi.models.entities.event.Event;
 import ar.utn.edu.frba.ddsi.models.entities.event.values.Category;
 import ar.utn.edu.frba.ddsi.models.entities.event.values.Origin;
 import ar.utn.edu.frba.ddsi.models.repositories.IEventRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,68 +20,61 @@ class EventRepositoryTest {
   @Autowired
   private IEventRepository eventRepository;
 
-  Event event0;
-  Event event1;
+  static LocalDateTime lastUpdate;
 
-  @BeforeEach
-  public void setUp() {
-    event0 = new Event(
-        "Event 0",
-        "Description 0",
-        new Category("Category 0"),
-        0.0,
-        0.0,
-        LocalDateTime.of(2023, 7, 27, 18, 15),
-        Origin.STATIC,
-        0L
-    );
-
-    event1 = new Event(
+  @BeforeAll
+  public static void init(@Autowired IEventRepository eventRepository) throws InterruptedException {
+    var event1 = new Event(
         "Event 1",
         "Description 1",
         new Category("Category 1"),
         0.0,
         0.0,
-        LocalDateTime.of(2022, 12, 12, 9, 58),
+        LocalDateTime.of(2023, 7, 27, 18, 15),
         Origin.STATIC,
         1L
     );
-    eventRepository.save(event0);
+
+    lastUpdate = LocalDateTime.now();
+
+    Thread.sleep(1);
+
+    var event2 = new Event(
+        "Event 2",
+        "Description 2",
+        new Category("Category 2"),
+        0.0,
+        0.0,
+        LocalDateTime.of(2022, 12, 12, 9, 58),
+        Origin.STATIC,
+        2L
+    );
+
     eventRepository.save(event1);
+    eventRepository.save(event2);
   }
 
   @Test
   @DisplayName("Se obtienen hechos no eliminados")
   public void testFindEvents() {
+    var event1 = eventRepository.findById(1L);
+    var event2 = eventRepository.findById(2L);
 
     var result = eventRepository.findByDeleted(false);
 
     assertNotNull(result);
     assertEquals(2, result.size());
-    assertEquals(event0, result.get(0));
-    assertEquals(event1, result.get(1));
+    assertEquals(event1, result.get(0));
+    assertEquals(event2, result.get(1));
   }
 
   @Test
   @DisplayName("Se obtienen hechos no actualizados")
   public void testFindAfterDate() {
 
-    LocalDateTime lastUpdate = LocalDateTime.now().minusNanos(1_000_000);
-
-    Event event2 = new Event(
-        "Event 2",
-        "Description 2",
-        new Category("Category 2"),
-        0.0,
-        0.0,
-        LocalDateTime.of(2021, 11, 12, 9, 58),
-        Origin.STATIC,
-        2L
-    );
-
-    eventRepository.save(event2);
-
+    var event2 = eventRepository.findById(2L);
     var result = eventRepository.findAfterDate(lastUpdate);
+
 
     assertNotNull(result);
     assertEquals(1, result.size());
@@ -90,15 +84,30 @@ class EventRepositoryTest {
   @Test
   @DisplayName("Se elimina un hecho")
   public void testDeleteEvent() {
-    var event = eventRepository.findById(1L);
+    var event1 = eventRepository.findById(1L);
+    var event2 = eventRepository.findById(2L);
+    var event3 = new Event(
+        "Event 3",
+        "Description 3",
+        new Category("Category 3"),
+        0.0,
+        0.0,
+        LocalDateTime.of(2024, 1, 1, 12, 0),
+        Origin.STATIC,
+        3L
+    );
+    eventRepository.save(event3);
+
+    var event = eventRepository.findById(3L);
     event.markAsDeleted();
     eventRepository.save(event);
 
     var events = eventRepository.findByDeleted(false);
 
     assertNotNull(events);
-    assertEquals(1, events.size());
+    assertEquals(2, events.size());
     assertEquals(event1, events.get(0));
+    assertEquals(event2, events.get(1));
   }
 
 }
