6f3b757e3e33d2916cfd00963fbeca0b4881b83b
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-25 22:03:15 -0300

    feat: Crear test para SourceService

---
+++ static-source/src/test/java/ar/utn/edu/frba/ddsi/services/impl/SourceServiceTest.java
@@ -0,0 +1,34 @@
+package ar.utn.edu.frba.ddsi.services.impl;
+
+import ar.utn.edu.frba.ddsi.models.dtos.input.SourceInputDTO;
+import ar.utn.edu.frba.ddsi.models.dtos.output.SourceOutputDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceServiceTest {
+
+  @Autowired
+  private SourceService sourceService;
+
+  @Test
+  @DisplayName("Se crea una fuente a partir de un DTO de entrada")
+  public void testCreateSource() {
+
+    // Arrange
+    SourceInputDTO dto = new SourceInputDTO();
+    dto.setFileName("sample_CSVtest_1.csv");
+    dto.setImportStrategy("CSV");
+
+    // Act
+    SourceOutputDTO result = sourceService.create(dto);
+
+    // Assert
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(1, result.getId());
+  }
+
+}
