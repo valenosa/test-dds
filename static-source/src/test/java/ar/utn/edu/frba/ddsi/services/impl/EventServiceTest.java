ff0d953674dbc5c20466b549fd57b9fa49666360
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-26 01:10:16 -0300

    feat: Agregar unit tests de EventService
    - Al pedir los hechos, se obtienen los no eliminados
    - Al pedir los hechos en base a la última update, se obtienen los no actualizados
    
    Co-authored-By: Mateo Nicolás Hernandez <mateonhernandez@frba.utn.edu.ar>

---
+++ static-source/src/test/java/ar/utn/edu/frba/ddsi/services/impl/EventServiceTest.java
@@ -0,0 +1,76 @@
+package ar.utn.edu.frba.ddsi.services.impl;
+
+
+import static org.mockito.Mockito.when;
+
+import ar.utn.edu.frba.ddsi.models.dtos.output.EventOutputDTO;
+import ar.utn.edu.frba.ddsi.models.entities.event.Event;
+import ar.utn.edu.frba.ddsi.models.entities.event.values.Category;
+import ar.utn.edu.frba.ddsi.models.entities.event.values.Origin;
+import ar.utn.edu.frba.ddsi.models.repositories.IEventRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+class EventServiceTest {
+
+  @Autowired
+  private EventService eventService;
+
+  @MockitoBean
+  private IEventRepository mockEventRepository;
+
+  Event event0 = new Event(
+      "Event 0",
+      "Description 0",
+      new Category("Category 0"),
+      0.0,
+      0.0,
+      LocalDateTime.of(2023, 7, 27, 18, 15),
+      Origin.STATIC,
+      0L
+  );
+  Event event1 = new Event(
+      "Event 1",
+      "Description 1",
+      new Category("Category 1"),
+      0.0,
+      0.0,
+      LocalDateTime.of(2022, 12, 12, 9, 58),
+      Origin.STATIC,
+      1L
+  );
+
+  @Test
+  @DisplayName("Al pedir los hechos, se obtienen los no eliminados")
+  public void testGetEvents() {
+    when(mockEventRepository.findByDeleted(false)).thenReturn(List.of(event0, event1));
+
+    var result = eventService.getEvents(null);
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(2, result.size());
+    Assertions.assertEquals(EventOutputDTO.from(event0), result.get(0));
+    Assertions.assertEquals(EventOutputDTO.from(event1), result.get(1));
+  }
+
+  @Test
+  @DisplayName("Al pedir los hechos en base a la última update, se obtienen los no actualizados")
+  public void testGetOutdatedEvents() {
+    var lastUpdate = LocalDateTime.of(2024, 5, 16, 14, 30);
+    when(mockEventRepository.findAfterDate(lastUpdate)).thenReturn(List.of(event1));
+    var result = eventService.getEvents(lastUpdate);
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(1, result.size());
+    Assertions.assertEquals(EventOutputDTO.from(event1), result.get(0));
+  }
+
+}
