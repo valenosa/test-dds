126e99611bf0c11c0c55d7f92848fa7155c55d76
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-05-26 18:38:31 -0300

    Style: Borro comentario

--- static-source/src/main/java/ar/utn/edu/frba/ddsi/StaticSourceApplication.java
+++ static-source/src/main/java/ar/utn/edu/frba/ddsi/StaticSourceApplication.java
@@ -3,8 +3,6 @@ package ar.utn.edu.frba.ddsi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
 public class StaticSourceApplication {
   public static void main(String[] args) {
