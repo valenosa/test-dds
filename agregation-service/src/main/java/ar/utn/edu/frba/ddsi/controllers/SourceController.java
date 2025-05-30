e90ab62582f514ce0fdf3a84918b082edce5458e
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-05-30 17:12:13 -0300

    feat: Agregar SourceClients dinámicamente
    - Permite la agregación dinámica de servicios de fuentes de datos (STATIC, DYNAMIC, PROXY) al Agregador.
    
    - La lógica de las requests a los servicios se delegó a cada SourceClient en cuestión
    
    - 👆 Esto permite que el SourceService solo se encargue de la orquestación (y simplifica la lógica del mismo)

---
+++ agregation-service/src/main/java/ar/utn/edu/frba/ddsi/controllers/SourceController.java
@@ -0,0 +1,27 @@
+package ar.utn.edu.frba.ddsi.controllers;
+
+import ar.utn.edu.frba.ddsi.models.dtos.input.SourceClientDTO;
+import ar.utn.edu.frba.ddsi.services.impl.SourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sources")
+public class SourceController {
+
+  @Autowired
+  SourceService sourceService;
+
+
+  @PostMapping("/clients")
+  public SourceClientDTO createSource(@RequestBody SourceClientDTO sourceClient) {
+    return sourceService.create(sourceClient);
+  }
+}
